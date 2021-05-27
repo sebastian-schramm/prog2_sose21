81237eb9c63c7d46d3a78728f90971de0824cd24
Oldsave <31001033+Oldsave@users.noreply.github.com>
2021-05-27 15:35:41 +0200

    Update Parser.java
    Joels Parser

--- teil_3/src/main/java/utilities/Parser.java
+++ teil_3/src/main/java/utilities/Parser.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import controller.PolyederController;
+import controller.TriangleController;
 import model.Triangle;
 import model.interfaces.AllgemeineKonstanten;
 import resources.StringKonstanten_DE;
@@ -13,12 +14,20 @@ import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class Parser {
 
-    public static Triangle[] erzeugePolyeder(String dateiName) {
-        return ladeStlAusDatei(dateiName);
+    public static Triangle[] erzeugePolyeder(String dateiName, int flag)
+    {
+        if (flag == 0){
+            return ladeStlAusDatei(dateiName);
+        }
+        else
+        {
+            return readAsciiFile(dateiName);
+        }
     }
 
     private static Triangle[] ladeStlAusDatei(String dateiName) {
@@ -34,10 +43,10 @@ public class Parser {
             CharBuffer decode = charset.decode(ByteBuffer.wrap(firstBlock, 0, 512));
             String buf = decode.toString().toLowerCase();
             StringBuffer sb = new StringBuffer();
-            int inl = r
 
 
-            System.out.println(buf);
+
+            System.out.println(buf); // Ausgabe
 
 //            byte[] header = new byte[80];
 //            byte[] b4=new byte[4];
@@ -68,6 +77,53 @@ public class Parser {
 //            e.printStackTrace();
 //        }
 
+        return null;
+    }
+
+    private static Triangle[] readAsciiFile(String dateiName)
+    {
+        String filePath = AllgemeineKonstanten.DEFAULT_RESOURCES_LOCATION + "" + dateiName + ".stl";
+        String word;
+        File file = new File(filePath);
+        ArrayList <String> relevantData = new ArrayList();
+        String vertex = AllgemeineKonstanten.VERTEX;
+        String facetNormal = AllgemeineKonstanten.FACET_NORMAL;
+        int count = 0;
+        if (file.canRead() && file.isFile())
+        {
+            try {
+                BufferedReader input = new BufferedReader(new FileReader(file));
+                while ((word = input.readLine()) != null)
+                {
+                    word = word.stripLeading();
+                    if (word.contains(vertex) || word.contains(facetNormal))
+                    {
+                        if (count < 4)
+                        {
+                            count += 1;
+                            relevantData.add(word);
+                        }
+                        else
+                        {
+                            TriangleController.getInstance().constructTriangle(relevantData);
+                            relevantData.clear();
+                            relevantData.add(word);
+                            count = 1;
+                        }
+                    }
+                }
+                input.close();
+                TriangleController.getInstance().constructTriangle(relevantData);
+            } catch (IOException e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
+        else
+        {
+            System.err.println("Die Datei ist nicht korrekt angeben");
+            System.exit(0);
+        }
         return null;
     }
 }
